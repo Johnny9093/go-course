15500a84deec2c6cad52ea65f417e999ac40e788
John Lahav <yoni9093@gmail.com>
2019-03-17 20:37:06 +0200

    more routines

--- go-routines/routines.go
+++ go-routines/routines.go
@@ -1,26 +1,44 @@
 package go_routines
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// use channels to communicate between different go routines
 	cn := make(chan int, 10)
 	// cn <- 2 // push value to channel
 
-	// run for as go routine
-	go func(inputChannel chan int) {
-		println("starting goroutine")
-		for {
-			println("pulling value")
-			value := <- inputChannel // since there are no values in the channel, the func is blocked here
-			// <- chan (pulling from chanel) is blocking.
-			println(value)
-		}
-	}(cn)
-	// exits immediately, we wont see the result
+	// 10 workers that will print their "id" and "task id".
+	// Subscriber
+	for i := 0; i < 10; i++ {
+		go func(index int, queue chan int) {
+			for {
+				fmt.Printf("Worker %d: %d\n", index, <- queue)
+			}
+		}(i, cn)
+	}
 
-	time.Sleep(3 * time.Second)
-	cn <- 13
+	// Publisher
+	for i := 0; i < 20; i++ {
+		cn <- i // will be blocked if no one empties the channel?
+	}
+
+	//// run for as go routine
+	//go func(inputChannel chan int) {
+	//	println("starting goroutine")
+	//	for {
+	//		println("pulling value")
+	//		value := <- inputChannel // since there are no values in the channel, the func is blocked here
+	//		// <- chan (pulling from chanel) is blocking.
+	//		println(value)
+	//	}
+	//}(cn)
+	//// exits immediately, we wont see the result
+
+	//time.Sleep(3 * time.Second)
+	//cn <- 13
 
 	time.Sleep(1 * time.Second)
 
